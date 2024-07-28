a6d67f0829939da4e78b05f3cc32c8147c26be47
Thomas Kottke <t.kottke90@gmail.com>
2024-07-28 10:44:42 -0500

    refactor: Add documentation to endpoints

--- src/controllers/prompt_controller.py
+++ src/controllers/prompt_controller.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Request, HTTPException
+from pydantic import BaseModel
 from src.dao import prompt_dao
 from src.models.prompt_model import CreatePromptDTO, PromptDTO, UpdatePromptDTO, UpdatePromptVersionDTO, PromptVersionDTO
 from src.models.query_params import DateFieldQueryModel, StringFieldQueryModel
 from src.utils import sqlalchemy_query, string_util, hateos_util
-from typing import List
 from src.services import evaluator
+from typing import List
 import logging
+import traceback
 
 controllerLogger = logging.getLogger('Controllers.PromptController')
 
@@ -13,7 +15,8 @@ PROMPTS_PATH = "/prompts"
 PROMPT_VERSION_PATH = "/prompts/{promptId}/versions"
 
 router = APIRouter(
-  prefix=PROMPTS_PATH
+  prefix=PROMPTS_PATH,
+  tags=["Prompts"]
 )
 
 def promptsPopulator(data: PromptDTO):
@@ -35,12 +38,12 @@ def addHatos(data: List[PromptDTO]):
   
   return data
 
-@router.post('/', status_code=201)
+@router.post('/', status_code=201, description="Create a new prompt in the database", name="Create New Prompt")
 def create(prompt: CreatePromptDTO):
   controllerLogger.debug('Creating New Prompt', extra={ "value": prompt.value, "label": prompt.label })
   return addHatos([prompt_dao.createPrompt(prompt)]).pop()
   
-@router.get('/')
+@router.get('/', description="Find a prompt", name="Prompt Search")
 def find(
   request: Request,
   skip: int = 0,
@@ -61,41 +64,73 @@ def find(
   except sqlalchemy_query.IndexOutOfBoundsError as boundsErr:
     raise HTTPException(400, 'BadRequest: Query Exceeded the number of items')
 
-@router.patch('/{prompt_id}')
+@router.patch('/{prompt_id}', description="Update prompt details", name="Update Prompt")
 def update(prompt_id: int, prompt: UpdatePromptDTO):
   return addHatos([
     prompt_dao.updatePrompt(prompt_id, prompt)
   ]).pop()
 
-@router.delete('/{prompt_id}', status_code=203)
+@router.delete('/{prompt_id}', status_code=203, description="Delete a Prompt")
 def remove(prompt_id: int):
   prompt_dao.deletePrompt(prompt_id)
 
-@router.post('/{promptId}/version/commit')
+@router.post('/{promptId}/version/commit', description="Save the current prompt changes as a version")
 def createRouterCommit(promptId: int):
   try:
     return addHatos([prompt_dao.addVersionCommit(promptId)]).pop()
   except prompt_dao.ValueHasNoChangeError as valueError:
-    controllerLogger.error('Prompt commit rejected - no changes detected', extra={ "promptId": promptId })
+    controllerLogger.error('Prompt commit rejected - no changes detected', extra={ "promptId": promptId, "source": "createRouterCommit" })
     raise HTTPException(status_code=400, detail=valueError.message)
   except IndexError as indexError:
-    controllerLogger.error('Prompt commit rejected - no changes detected', extra={ "promptId": promptId, "rootError": indexError.__repr__() })
+    controllerLogger.error('Prompt commit rejected - no changes detected', extra={ "promptId": promptId, "rootError": indexError.__repr__(), "source": "createRouterCommit" })
     raise HTTPException(status_code=404, detail="Not Found")
   except Exception as e:
-    controllerLogger.error(f'Prompt commit rejected - {type(e)}', extra={ "promptId": promptId, "rootError": indexError.__repr__() })
+    controllerLogger.error(f'Prompt commit rejected - {type(e)}', extra={ "promptId": promptId, "rootError": indexError.__repr__(), "source": "createRouterCommit" })
     raise HTTPException(status_code=404, detail="Not Found")
  
-@router.get('/{promptId}/evaluator/inputs')
+@router.get('/{promptId}/evaluator/inputs', description="Get a prompt's template inputs")
 def getPromptInputs(promptId: int):
   try:
     prompt = prompt_dao.getPrompt(promptId)
 
-    return evaluator.getPromptInputs(prompt.value)
+    return evaluator.getPromptInputs(prompt)
 
   except IndexError as indexError:
     print(indexError)
-    controllerLogger.error('Prompt commit rejected - no changes detected', extra={ "promptId": promptId, "rootError": indexError.__repr__() })
+    controllerLogger.error('Prompt commit rejected - no changes detected', extra={ "promptId": promptId, "rootError": indexError.__repr__(), "source": "getPromptInputs" })
     raise HTTPException(status_code=404, detail="Not Found")
   except Exception as e:
-    controllerLogger.error(f'Prompt commit rejected - {type(e)}', extra={ "promptId": promptId, "rootError": e.__repr__() })
+    controllerLogger.error(f'Prompt commit rejected - {type(e)}', extra={ "promptId": promptId, "rootError": e.__repr__(), "source": "getPromptInputs" })
     raise HTTPException(status_code=404, detail="Not Found")
+  
+@router.post('/{promptId}/evaluator/test', name="Generate LLM Response from Prompt", description="Execute a prompt with the given input values")
+def getPromptInputs(promptId: int, body: dict):
+  try:
+    prompt = prompt_dao.getPrompt(promptId)
+
+    return evaluator.testPrompt(prompt, [], body)
+
+  except HTTPException as httpException:
+    raise httpException
+  except Exception as e:
+    controllerLogger.error(f'Prompt commit rejected - {type(e)}', extra={ "promptId": promptId, "rootError": e.__repr__(), "source": "getPromptInputs", "trace": traceback.format_exception(e) })
+    raise HTTPException(status_code=500, detail="Server Error")
+  
+@router.post('/{promptId}/evaluator/score', name="Score LLM Response", description="Execute an LLM Generation with the given prompt. Then ask the LLM to score the response and provide feedback")
+def getPromptInputs(promptId: int, body: dict):
+  try:
+    prompt = prompt_dao.getPrompt(promptId)
+
+    (result, template) = evaluator.testPrompt(prompt, [], body)
+
+    return {
+      "result": result,
+      "template": template,
+      "score": evaluator.scoreGeneration(template, result)
+    }
+
+  except HTTPException as httpException:
+    raise httpException
+  except Exception as e:
+    controllerLogger.error(f'Prompt commit rejected - {type(e)}', extra={ "promptId": promptId, "rootError": e.__repr__(), "source": "getPromptInputs", "trace": traceback.format_exception(e) })
+    raise HTTPException(status_code=500, detail="Server Error")
--- src/controllers/root.py
+++ src/controllers/root.py
@@ -1,7 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Cookie, Response
+from typing import Annotated
+from datetime import datetime
 
-router = APIRouter()
+router = APIRouter(
+  tags=["Application"]
+)
 
-@router.get('/healthcheck')
+@router.get('/healthcheck', name="Healthcheck Endpoint", description="Utility endpoint to check if the API is healthy")
 def healthcheck_endpoint() -> str:
+  
   return "OKAY"
+
+
+@router.get('/teapot', status_code=418, name="Teapot Endpoint", description="Endpoint for returning the 418 - Teapot error status")
+def healthcheck_endpoint(response: Response, teapotAchievementCookie: Annotated[str | None, Cookie(include_in_schema=False)] = None) -> str:
+  if (isinstance(teapotAchievementCookie, str)):
+    return "You found nothing looking for the teapot"
+
+
+  response.set_cookie(key="teapotAchievementCookie", value='Achievement Unlocked on {:%Y-%m-%d %H:%M:%S}'.format(datetime.now()))
+  return "Achievement Get: Find the Teapot"
